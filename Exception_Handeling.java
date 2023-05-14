fd2626f417e1866dbf04b57ad7ac8eaed20670d3
AbrarZaved <abrarzaved2002@gmail.com>
2023-05-14 10:01:42 +0600

    New File

---
+++ Exception_Handeling.java
@@ -0,0 +1,14 @@
+public class Exception_Handeling{
+    public static void main(String[] args) {
+        try{
+            int x=10;
+            int y=0;
+            int result = x/y;
+            System.out.println("Result: "+result);
+        }catch(ArithmeticException e){
+            System.out.println("Exception: "+e);
+        }
+       
+       System.out.println("Last line of the program"); 
+    }
+}
