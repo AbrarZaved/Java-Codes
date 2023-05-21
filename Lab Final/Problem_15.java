a025cfbad29fab98096423f3c08d64a5f490634d
AbrarZaved <abrarzaved2002@gmail.com>
2023-05-21 11:11:02 +0600

    update

--- Lab Final/Problem_15.java
+++ Lab Final/Problem_15.java
@@ -4,9 +4,10 @@ public class Problem_15 {
     public static void main(String[] args) {
         int a;
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter value of N: ");
+        System.out.print("Enter value of N: ");
         a=in.nextInt();
         int fibo=0,rem=1;
+        System.out.println("Fibonacci Series");
         System.out.print("0");
         for(int i=1;i<a;i++){
             int sum=fibo+rem;
