b592240b574d2b8d8f1204983ddaa875c8ddcd53
AbrarZaved <abrarzaved2002@gmail.com>
2023-05-21 11:09:02 +0600

    update

--- Lab Final/Problem_15.java
+++ Lab Final/Problem_15.java
@@ -6,9 +6,9 @@ public class Problem_15 {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter value of N: ");
         a=in.nextInt();
-        int fibo=1,rem=1;
-        System.out.print("0,1,1");
-        for(int i=4;i<=a;i++){
+        int fibo=0,rem=1;
+        System.out.print("0");
+        for(int i=1;i<a;i++){
             int sum=fibo+rem;
             rem=fibo;
             fibo=sum;
