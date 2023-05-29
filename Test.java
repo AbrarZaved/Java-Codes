a57bcf396c19197352f9bb5759e83380c4da1c4a
AbrarZaved <abrarzaved2002@gmail.com>
2023-05-29 19:14:48 +0600

    test file

---
+++ Test.java
@@ -0,0 +1,20 @@
+public class Test
+{
+public static void main(String[] args)
+{
+try
+{
+System.out.printf("1");
+int data = 5 / 0;
+}
+catch(NullPointerException e)
+{
+System.out.printf("2");
+}
+finally
+{
+System.out.printf("3");
+}
+System.out.printf("4");
+}
+}
